cd2d2874c54fa0f686b55588bd1a48a11622be18
Egidya <salsabilaggiya@gmail.com>
2022-12-29 20:40:57 +0700

    done

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://egidya-finalproject.web.app" // TODO: replace this
+const studentName = "Egidya Zalsabila" // TODO: replace this
+const studentId = "FE3162403" // TODO: replace this
+
+export { url, studentName, studentId }
